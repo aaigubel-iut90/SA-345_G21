2d9f2ce6c6f17507fec572fa51e9dd089a74a78e
Nathan PONTHIEU <nathan.ponthieu@edu.univ-fcomte.fr>
2024-02-25 17:46:42 +0100

    update

--- controllers/admin_commande.py
+++ controllers/admin_commande.py
@@ -32,9 +32,35 @@ def admin_commande_show():
     commande_adresses = None
     id_commande = request.args.get('id_commande', None)
     print(id_commande)
-    if id_commande != None:
-        sql = '''    '''
-        commande_adresses = []
+    if id_commande is not None:
+        print(id_commande)
+        sql = '''SELECT * , (quantite * prix) AS prix_ligne FROM ligne_commande WHERE commande_id = %s'''
+        mycursor.execute(sql, (id_commande,))
+        articles_commande = mycursor.fetchall()
+
+        # partie 2 : selection de l'adresse de livraison et de facturation de la commande selectionnée
+        sql_commande_adresses = '''
+        SELECT 
+        *,
+        a1.nom AS nom_livraison, 
+        a1.rue AS rue_livraison, 
+        a1.code_postal AS code_postal_livraison, 
+        a1.ville AS ville_livraison, 
+        a2.nom AS nom_facturation, 
+        a2.rue AS rue_facturation, 
+        a2.code_postal AS code_postal_facturation, 
+        a2.ville AS ville_facturation
+    FROM 
+        commande
+    LEFT JOIN 
+        adresse a1 ON commande.adresse = a1.id_adresse
+    LEFT JOIN 
+        adresse a2 ON commande.adresse_1 = a2.id_adresse
+    WHERE 
+        commande.id_commande = %s 
+        '''
+        mycursor.execute(sql_commande_adresses, (id_commande,))
+        commande_adresses = mycursor.fetchone()
     return render_template('admin/commandes/show.html'
                            , commandes=commandes
                            , articles_commande=articles_commande
@@ -48,7 +74,7 @@ def admin_commande_valider():
     commande_id = request.form.get('id_commande', None)
     if commande_id != None:
         print(commande_id)
-        sql = '''           '''
+        sql = '''UPDATE commande SET etat_id = 2 WHERE id_commande = %s'''
         mycursor.execute(sql, commande_id)
         get_db().commit()
     return redirect('/admin/commande/show')
